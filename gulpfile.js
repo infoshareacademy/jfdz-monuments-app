ef72becd2eda9513e117d4448c3bc92ebf53102e
konrad5 <konradkc@op.pl>
2016-04-03 17:15:37 +0200

    gulp jade instalacja pomyslna

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+/**
+ * Created by konrad5 on 03.04.16.
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var jade = require('gulp-jade');
+
+
+gulp.task('templates', function() {
+    gulp.src('./src/jade/*.jade')
+        .pipe(jade({
+            pretty: true,
+            locals: {
+                title: 'My website'
+            }
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('templates:watch', ['templates'], function () {
+    gulp.watch('./src/jade/**/*.jade', ['templates']);
+});
+
+
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/**/*')
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:watch', ['sass'], function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass:watch', 'templates:watch']);
