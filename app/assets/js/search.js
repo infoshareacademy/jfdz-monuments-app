a15e1e793ae66363d9caef40b3c2f877d47a9c88
mplocki <plockione@gmail.com>
2016-05-08 18:32:22 +0200

    wyszukiwarka2

--- app/assets/js/search.js
+++ app/assets/js/search.js
@@ -1,16 +1,42 @@
 (function () {
-    var app = angular.module ('app', []);
-    app.controller('UsersController', ['$scope', '$http', function ($scope, $http) {
-        $http.get('../dist/data/zabytki.json').success(function (data) {
-            $scope.zabytki = data;
-        });
 
-        $scope.$watch('selectedMonumentId', function () {
-            var filtered = $scope.zabytki.filter(function (item) {
-                return item.id  === $scope.selectedMonumentId;
+    var app = angular.module('app', []);
+    angular.module('app', ['ngRoute']);
+    angular.module('app',['ngResource']);
+
+    app.controller('UsersController', ['$scope', '$http', function ($scope, $http) {
+        $http.get('../dist/data/zabytki_2.csv').success(function (data) {
+            $scope.zabytki = data;
+
+            var arr = CSVToArray(data, ';');
+
+            var mojeZabytyki = arr.filter(function (item) {
+                for (var i = 0; i < item.length; i++) {
+                    if (item[i].indexOf('kościół') != -1) {
+                        return true;
+                    }
+                }
+
+                return false;
             });
 
-            $scope.selectedMonument = filtered.length > 0 ? filtered[0] : null;
+            mojeZabytyki = mojeZabytyki.map(function (item) {
+                return {
+                    address: item[1] + ' ' + item[2],
+                    description: item[3]
+                }
+            });
+
+            console.log(mojeZabytyki);
+
+            //$scope.$watch('selectedMonumentId', function () {
+            //    var filtered = $scope.zabytki.filter(function (item) {
+            //        return item.id  === $scope.selectedMonumentId;
+            //    });
+            //
+            //    $scope.selectedMonument = filtered.length > 0 ? filtered[0] : null;
+            //});
         });
+
     }]);
-})();
+});
--- dist/js/search.js
+++ dist/js/search.js
@@ -1,19 +1,37 @@
 (function () {
-    var app = angular.module ('app', []);
+    var app = angular.module('app', []);
     app.controller('UsersController', ['$scope', '$http', function ($scope, $http) {
         $http.get('../dist/data/zabytki_2.csv').success(function (data) {
             $scope.zabytki = data;
 
             var arr = CSVToArray(data, ';');
-            console.log(arr);
 
-            $scope.$watch('selectedMonumentId', function () {
-                var filtered = $scope.zabytki.filter(function (item) {
-                    return item.id  === $scope.selectedMonumentId;
-                });
+            var mojeZabytyki = arr.filter(function (item) {
+                for (var i = 0; i < item.length; i++) {
+                    if (item[i].indexOf('kościół') != -1) {
+                        return true;
+                    }
+                }
 
-                $scope.selectedMonument = filtered.length > 0 ? filtered[0] : null;
+                return false;
             });
+
+            mojeZabytyki = mojeZabytyki.map(function (item) {
+                return {
+                    address: item[1] + ' ' + item[2],
+                    description: item[3]
+                }
+            });
+
+            console.log(mojeZabytyki);
+
+            //$scope.$watch('selectedMonumentId', function () {
+            //    var filtered = $scope.zabytki.filter(function (item) {
+            //        return item.id  === $scope.selectedMonumentId;
+            //    });
+            //
+            //    $scope.selectedMonument = filtered.length > 0 ? filtered[0] : null;
+            //});
         });
 
     }]);
