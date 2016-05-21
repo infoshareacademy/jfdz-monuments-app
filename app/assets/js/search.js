345f1a162b6cb4d5dfe3cd5f7561fa6626e02d55
mplocki <plockione@gmail.com>
2016-05-21 15:42:54 +0200

    nowa wersja nowa wersja

--- app/assets/js/search.js
+++ app/assets/js/search.js
@@ -1,42 +1,38 @@
-(function () {
+var app = angular.module('app', []);
 
-    var app = angular.module('app', []);
-    angular.module('app', ['ngRoute']);
-    angular.module('app',['ngResource']);
+app.controller('UsersController', ['$scope', '$http', function ($scope, $http) {
+    $http.get('../dist/data/zabytki_2.csv').success(function (data) {
+        $scope.kategorieZabytkow = ['kościół'];
+        $scope.zabytki = data;
 
-    app.controller('UsersController', ['$scope', '$http', function ($scope, $http) {
-        $http.get('../dist/data/zabytki_2.csv').success(function (data) {
-            $scope.zabytki = data;
+        var arr = CSVToArray(data, ';');
 
-            var arr = CSVToArray(data, ';');
-
-            var mojeZabytyki = arr.filter(function (item) {
-                for (var i = 0; i < item.length; i++) {
-                    if (item[i].indexOf('kościół') != -1) {
-                        return true;
-                    }
-                }
-
-                return false;
-            });
-
-            mojeZabytyki = mojeZabytyki.map(function (item) {
-                return {
-                    address: item[1] + ' ' + item[2],
-                    description: item[3]
-                }
-            });
-
-            console.log(mojeZabytyki);
-
-            //$scope.$watch('selectedMonumentId', function () {
-            //    var filtered = $scope.zabytki.filter(function (item) {
-            //        return item.id  === $scope.selectedMonumentId;
-            //    });
-            //
-            //    $scope.selectedMonument = filtered.length > 0 ? filtered[0] : null;
-            //});
+        var mojeZabytyki = arr.filter(function (item) {
+            console.log(item[3], item[3].indexOf('kościół'));
+            return item[3].indexOf('kościół') !== -1;
         });
 
-    }]);
-});
+        mojeZabytyki = mojeZabytyki.map(function (item) {
+            return {
+                address: item[1] + ' ' + item[2],
+                description: item[3]
+            }
+        });
+
+        $scope.zabytki = mojeZabytyki;
+
+        ng repeat
+
+        //$scope.$watch('selectedMonumentId', function () {
+        //    var filtered = $scope.zabytki.filter(function (item) {
+        //        return item.id  === $scope.selectedMonumentId;
+        //    });
+        //
+        //    $scope.selectedMonument = filtered.length > 0 ? filtered[0] : null;
+        //});
+    });
+
+    $scope.onCategoryChange = function () {
+        console.log('zmiana kategorii, na ', $scope.selectedCategory);
+    }
+}]);
