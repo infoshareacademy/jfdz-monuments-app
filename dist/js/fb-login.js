c54fae9b6558b7a9fa7a5b5998ced0515d358ecd
Halina <hmd@wp.pl>
2016-05-21 17:37:10 +0200

    fb

--- dist/js/fb-login.js
+++ dist/js/fb-login.js
@@ -35,7 +35,7 @@ window.fbAsyncInit = function () {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s);
-    js.id = 1730287713922335;
+    js.id = id;
     js.src = "//connect.facebook.net/en_US/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
