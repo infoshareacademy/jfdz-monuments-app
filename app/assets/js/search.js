243d24a9cb1674a17e17f5d8e79aa79435d838ee
konrad5 <konradkc@op.pl>
2016-05-22 15:40:42 +0200

    reakcja menu na click (angular)

--- app/assets/js/search.js
+++ app/assets/js/search.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['touristApp']);
+var app = angular.module('app', ['anchorApp','touristApp']);
 
 app.controller('UsersController', ['$scope', '$http', function ($scope, $http) {
     $scope.zabytki = {};
