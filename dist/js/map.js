d8d7fbed8fc502e2d13598f30c328e67fdc191a9
Konrad5 <konradkc@op.pl>
2016-06-01 00:08:01 +0200

    code review

--- dist/js/map.js
+++ dist/js/map.js
@@ -19,15 +19,6 @@ function init_map(){var myOptions = {
     marker4 = new google.maps.Marker({map: map,position: new google.maps.LatLng(54.3498348,18.653284699999972)});
     marker5 = new google.maps.Marker({map: map,position: new google.maps.LatLng(54.34854499999999,18.653229499999952)});
 
-    //okno na mapie po klikeniciu
-    //infowindow = new google.maps.InfoWindow({content:'<strong>STOCZNIA GDANSKA</strong><br>Gdansk, Jana z Kolna<br>'});
-    //infowindow = new google.maps.InfoWindow({content:'<strong>ZURAW</strong><br>Gdansk, Szeroka 67/68<br>'});
-    //infowindow = new google.maps.InfoWindow({content:'<strong>DWOR ARTUSA</strong><br>Gdansk, Dlugi Targ 43-44<br>'});
-    //infowindow = new google.maps.InfoWindow({content:'<strong>GDANSKI DWORZEC</strong><br>Gdansk, Podwale Grodzkie 2<br>'});
-    //infowindow = new google.maps.InfoWindow({content:'<strong>BAZYLIKA MARIACKA</strong><br>Gdansk, Podkramarska 5<br>'});
-    //infowindow = new google.maps.InfoWindow({content:'<strong>FONTANNA NEPTUNA</strong><br>Gdansk, Dlugi Targ<br>'});
-
-
     google.maps.event.addListener(marker, 'click', function(){
         $('div.zabytek.photo_stocznia').show();
         $('div.zabytek.photo_zuraw').hide();
@@ -78,22 +69,7 @@ function init_map(){var myOptions = {
         $('div.zabytek.photo_zuraw').hide();
         $('div.zabytek.photo_stocznia').hide();
     });
-    //google.maps.event.addListener(marker1, 'click', function(){
-    //    var monument2= document.getElementById('zabytek');
-    //    monument2.innerHTML ="moja chata";
-    //});
-    //google.maps.event.addListener(marker, 'click', function(){infowindow.open(map,marker);});
-    //infowindow.open(map,marker);
-    //infowindow.open(map,marker1);
-    //infowindow.open(map,marker2);
-    //infowindow.open(map,marker3);
-    //infowindow.open(map,marker4);
-    //infowindow.open(map,marker5);
+
 }
     google.maps.event.addDomListener(window, 'load', init_map);
 
-
-//google.maps.on('click', marker, function(){
-//  $('.plan_wycieczki').text('jestem tutaj');
-//
-//});
--- src/js/test.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by konrad5 on 16.04.16.
- */
-var app = angular.module("app", []);
-
-app.controller('HomeCtrl', function ($scope) {
-    $scope.name = 'Potworze';
-});
